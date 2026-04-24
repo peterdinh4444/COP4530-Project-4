fcf5d6fdc8e71005a103909dc7b568450483b229
Peter <peterdd06@gmail.com>
2026-04-24 19:23:22 -0400

    vertex file done

--- vertex.cpp
+++ vertex.cpp
@@ -15,15 +15,23 @@ vector<Edge*> Vertex::incidentEdges(){
 
 //function to check if v is a neighbor to current vertex
 bool Vertex::isAdjacentTo(Vertex* v){
-    
+    for(Edge* e: adjEdges){
+        if (e->opposite(this) == v) return true;
+    }
+    return false;
 }
 
 //function to insert an edge
 void Vertex::addEdge(Edge* e){
-
+    adjEdges.push_back(e);
 }
 
 //function to remove an Edge
 void Vertex::removeEdge(Edge* e){
-
+    for(auto it = adjEdges.begin(); it != adjEdges.end(); ++it){
+        if (*it == e) {
+            adjEdges.erase(it);
+            return;
+        }
+    }
 }
